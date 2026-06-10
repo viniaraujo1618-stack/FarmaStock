58186dcf2fabdd90866679f23c6d66d110ac1724
Vinicius Araujo <viniaraujo1618@gmail.com>
2026-06-09 23:35:27 -0300

    Project update
    git push origin mai#

--- Farmastock.py
+++ Farmastock.py
@@ -1,9 +1,44 @@
-from flask import Flask
-from pip._internal.commands import index
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///farmastock.db'
+
+db = SQLAlchemy(app)
+class Product (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    
 
 
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
 
+    if 'name' in data and 'price' in data:
+        product = Product(
+            name=data["name"],
+            price=data["price"],
+            description=data.get("description", "")
+        )
+
+        db.session.add(product)
+        db.session.commit()
+
+        return jsonify({"message": "Product added successfully"}), 200
+
+    return jsonify({"message": "Invalid product data"}), 400
+@app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"}), 200
+
+    return jsonify({"message": "Product not found"}), 404
 produtos = [
     {"nome": "Dipirona", "quantidade": 15},
     {"nome": "Paracetamol", "quantidade": 20},
@@ -35,6 +70,6 @@ def listar_produtos():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    README.md
+  
 
 
